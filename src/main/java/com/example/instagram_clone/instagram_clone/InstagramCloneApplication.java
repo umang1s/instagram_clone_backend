8b539ba2ecab175d60829287561b667f0fb3485f
Umang Maurya <umang1maurya.com>
2022-09-25 09:59:41 +0530

    initial setup

---
+++ src/main/java/com/example/instagram_clone/instagram_clone/InstagramCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.instagram_clone.instagram_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramCloneApplication.class, args);
+	}
+
+}
